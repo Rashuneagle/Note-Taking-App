524613d94ba518f4dabe865bfc5cb9670fc355a0
Michael Scott <mscotteagle21@gmail.com>
2024-03-10 21:55:33 -0500

    added routes, updated gitignore.

--- Develop/server.js
+++ Develop/server.js
@@ -1,5 +1,26 @@
 const express = require('express');
 const fs = require('fs');
 const uuid = require('uuid');
+const notesData = require('./db/db.json')
+const PORT = process.env.PORT || 3000
 
 const app = express();
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'notes.html'));
+  });
+
+
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  });
+
+  app.get('/api/notes', (req, res) => res.json(notesData));
+
+  app.post('/api/notes', (req, res) => res.json(notesData));
+
+
+  app.listen(PORT, () => {
+    console.log(`Example app listening at http://localhost:${PORT}`);
+  });
